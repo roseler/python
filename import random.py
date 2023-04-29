88ee902449293919e89a759b308dd60f3486b4ed
not roseler <93562457+roseler@users.noreply.github.com>
2023-04-29 09:45:10 +0800

    Update import random.py

--- import random.py
+++ import random.py
@@ -1,14 +1,51 @@
 import random
 
-points =  1000
+#For Number 1
+
+points = 1000000
 points_in_circle = 0
 
-for i in range (points):
-    x = random.uniform(-1,1)
-    y = random.uniform(-1,1)
+for i in range(points):
+    x = random.random()
+    y = random.random()
+    if x**2 + y**2 <= 1:
+        points_in_circle += 1
 
-    if x**2 + y**2 <=1
+print("Pi is approximately", 4 * points_in_circle / points)
 
-pistemate = 4 * points_in_circle / points
 
-print ("Estimated value of pi with", points, " points: ", pistemate)
+#For Number 2
+
+num_points = 100000
+points_under_curve = 0
+
+p1 = (-1, 2)
+p2 = (2, -2)
+
+for i in range(num_points):
+    x = random.uniform(p1[0], p2[0])
+    y = random.uniform(p1[1], p2[1])
+    if y <= x**2 and x >= p1[0] and x <= p2[0]:
+        points_under_curve += 1
+        
+area = points_under_curve / num_points * (p2[0] - p1[0]) * (p2[1] - p1[1])
+
+print("Area is approximately", area)
+
+#For Number 3
+
+pointss = 100000
+points_inside_ellipsoid = 0
+
+a, b, c = 2, 3, 4
+
+for i in range(pointss):
+    x = random.uniform(-a, a)
+    y = random.uniform(-b, b)
+    z = random.uniform(-c, c)
+    if x*x/a + y*y/b + z*z/c <= 1:
+        points_inside_ellipsoid += 1
+    
+volume = points_inside_ellipsoid / pointss * 8 * a * b * c
+
+print("Volume is approximately", volume)
