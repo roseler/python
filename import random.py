0f5c0b849627fd10c915bd5104b859dc1fa95ce2
not roseler <93562457+roseler@users.noreply.github.com>
2023-04-29 08:41:09 +0800

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,14 @@
+import random
+
+points =  1000
+points_in_circle = 0
+
+for i in range (points):
+    x = random.uniform(-1,1)
+    y = random.uniform(-1,1)
+
+    if x**2 + y**2 <=1
+
+pistemate = 4 * points_in_circle / points
+
+print ("Estimated value of pi with", points, " points: ", pistemate)
